6a9f779699626382c33d8ec0a8d8c82c53c06ef6
Tei Seibun <teiseibun.tw@gmail.com>
2020-02-28 00:16:13 +0800

    plot datas

--- quadrotor_sim.m
+++ quadrotor_sim.m
@@ -1,4 +1,6 @@
 function quadrotor_sim
+	ITERATION_TIMES = 10000
+
 	math = se3_math;
 
 	uav_dynamics = dynamics;    %create uav dynamics object
@@ -17,7 +19,23 @@ function quadrotor_sim
 			  0 1 0;
 			  0 0 1];
 
-	for i = 1: 10000
+	%plot datas
+	time_arr = zeros(1, ITERATION_TIMES);
+	pos_a_arr = zeros(1, ITERATION_TIMES);
+	pos_v_arr = zeros(1, ITERATION_TIMES);
+	pos_x_arr = zeros(1, ITERATION_TIMES);
+
+	for i = 1: ITERATION_TIMES
 		uav_dynamics = update(uav_dynamics);
+		
+		pos_a_arr(i) = uav_dynamics.a(1);
+		pos_v_arr(i) = uav_dynamics.v(1);
+		pos_x_arr(i) = uav_dynamics.x(1);
+
+		time_arr(i) = i * uav_dynamics.dt;
 	end
+
+	figure(1)
+	plot(time_arr, pos_x_arr);
+	pause;
 end
